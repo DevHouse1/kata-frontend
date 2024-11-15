498657e842e0e06cd6a02e172407793b2e98373d
houssam houriem <houssam_houriem@hotmail.fr>
2024-11-15 07:37:56 +0100

    update routes in  App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+//import GlobalStyles from './styles/GlobalStyles';
+import Home from './pages/Home';
+import GlobalProvider from './contexts/GlobalContext';
+import GlobalStyles from './styles/GlobalStyle';
+import CommandList from './pages/CommandList';
+//import CommandStatus from './pages/CommandStatus';
 
 function App() {
   return (
-    <div className="App">
-       hello from react
-    </div>
+    <GlobalProvider>
+     <GlobalStyles />
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/commands">Mes Commandes</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/commands" element={<CommandList />} />
+        </Routes>
+      </Router>
+    </GlobalProvider>
   );
 }
 
